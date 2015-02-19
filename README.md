e73d0ae6ac4b6055c80c6602196fceeb4c9bfb08
Jan Szpila <jszpila@gmail.com>
2015-02-19 11:48:31 -0800

    Initial commit
    Do the thing

---
+++ README.md
@@ -0,0 +1,13 @@
+# WWIDataVis
+A quick project to monkey around with some of the features of [D3](http://d3js.org/) and [Dimple](http://dimplejs.org/), based on some exercises during a presentation by [Jonathan Dinu](https://github.com/jay-oh-en) at [Strata + Hadoop 2015](http://strataconf.com/big-data-conference-ca-2015). Data taken from [about.com](http://europeanhistory.about.com/cs/worldwar1/a/blww1casualties.htm), note that some of the numbers don't quite add up.
+
+## Up & Running
+### Requirements
+
+- [Bower](http://bower.io/)
+
+### Installation
+
+	$ git clone https://github.com/jszpila/WWIDataVis WWIDataVis && cd WWIDataVis
+	$ bower install
+	$ python -m SimpleHTTPServer
---
+++ js/main.js
@@ -0,0 +1,78 @@
+(function() {
+  $(function() {
+    var $country = $('#display-country'),
+      $type = $('#display-type'),
+      w = $('#chart-container').width(),
+      data = null;
+
+    $country.on('change', updateChart);
+    $type.on('change', updateChart);
+
+    function updateChart() {
+      d3.select('svg').remove();
+
+      var country = $country.val(),
+        type = $type.val(),
+        subset = null,
+        categories = ['Country'],
+        plot = dimple.plot.bar,
+        total = 0,
+        notOk = 0;
+
+      if (country !== 'all') {
+        subset = $.grep(data, function(v) {
+          if (v.Country === country) {
+            console.log(country, v.Type, ': ', v.People);
+            if (v.Type !== 'Deployed') {
+              notOk += parseInt(v.People);
+              return true;
+            } else {
+              total = parseInt(v.People);
+              return false;
+            }
+          } else {
+            return false;
+          }
+        });
+
+        console.log(total, notOk, total - notOk);
+        subset.push({Country: country, Type: 'OK', People: total - notOk});
+
+        plot = dimple.plot.pie;
+        $type.attr('disabled','disabled');
+      } else {
+        subset = data;
+        $type.removeAttr('disabled');
+      }
+
+      if (type !== 'all') {
+        subset = $.grep(subset, function(v) {
+          return v.Type === type;
+        });
+      } else {
+        categories.push('Type');
+      }
+
+      var svg = dimple.newSvg('#chart-container', w, 500),
+        myChart = new dimple.chart(svg, subset);
+
+      if (plot === dimple.plot.bar) {
+        myChart.addCategoryAxis('x', categories);
+        myChart.addMeasureAxis('y', 'People');
+        myChart.addLegend(0, 10, 325, 20, 'center');
+      } else {
+        myChart.addMeasureAxis('p', 'People');
+        myChart.addLegend(0, 10, 100, 100, 'center');
+      }
+
+      myChart.addSeries('Type', plot);
+      myChart.setBounds(50, 30, w, 340);
+      myChart.draw();
+    };
+
+    d3.csv('data/data.csv', function(d) {
+      data = d;
+      updateChart();
+    });
+  });
+}).call(this);
